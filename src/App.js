ec1e3af7949976db265990d861e4da5182521df7
Ankit-Developer143 <dante7785@gmail.com>
2022-11-22 14:28:04 +0530

    folder structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+    <GlobalStyles />
+    App file
+    </>
+  );
+}
+
+export default App;
